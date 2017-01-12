4c6db57f7b9822384aaef55eb60b165e6a7a145a
SoponThaiou <soponthaiou@gmail.com>
2017-01-12 14:38:42 +0700

    Edit 01

--- app/src/main/java/th/ac/met/nodemcu_controlled/MainActivity.java
+++ app/src/main/java/th/ac/met/nodemcu_controlled/MainActivity.java
@@ -1,7 +1,10 @@
 package th.ac.met.nodemcu_controlled;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButtonClick(View v) {
+        if (v.getId() == R.id.button) {
+            TextView resualt = (TextView) findViewById(R.id.button);
+            promptSpeechInput();
+        }
+    }
+
+    private void promptSpeechInput() {
+        Intent i = new Intent(RecognizerIntente)
+    }
 }
+
